fdbcf38d1625292d772d9585329d496238ecc549
Lau E. Ewertsen <laewe19@student.sdu.dk>
2024-01-22 09:21:10 +0100

    Changes

--- Assets/Scripts/Layer_Blocks_Controller.cs
+++ Assets/Scripts/Layer_Blocks_Controller.cs
@@ -2,12 +2,22 @@ using System;
 using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
+using UnityEngine.Windows;
+using static Unity.VisualScripting.Member;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Layer_Blocks_Controller : MonoBehaviour
 {
-
+    [SerializeField] GameObject LayerTypePrefab_0;
     [SerializeField] GameObject LayerTypePrefab_1;
     [SerializeField] GameObject LayerTypePrefab_2;
     [SerializeField] GameObject LayerTypePrefab_3;
@@ -20,6 +30,8 @@ public class Layer_Blocks_Controller : MonoBehaviour
     [SerializeField] GameObject LayerPos_4;
     [SerializeField] GameObject LayerPos_5;
 
+    GameObject[] LayerPos;
+
     enum LayerTypes
     {
         Dirt_Layer,
@@ -29,102 +41,115 @@ public class Layer_Blocks_Controller : MonoBehaviour
         Clay_Layer
     }
 
-    [SerializeField] LayerTypes LayerTypes_1 = new();
-    [SerializeField] LayerTypes LayerTypes_2 = new();
-    [SerializeField] LayerTypes LayerTypes_3 = new();
-    [SerializeField] LayerTypes LayerTypes_4 = new();
-    [SerializeField] LayerTypes LayerTypes_5 = new();
+    [SerializeField] LayerTypes Layer_1_Type = new();
+    [SerializeField] LayerTypes Layer_2_Type = new();
+    [SerializeField] LayerTypes Layer_3_Type = new();
+    [SerializeField] LayerTypes Layer_4_Type = new();
+    [SerializeField] LayerTypes Layer_5_Type = new();
 
-    GameObject layer_1;
-    GameObject layer_2;
-    GameObject layer_3;
-    GameObject layer_4;
-    GameObject layer_5;
-
-    string layer_1_SavedType;
-    string layer_2_SavedType;
-    string layer_3_SavedType;
-    string layer_4_SavedType;
-    string layer_5_SavedType;
-
+    LayerTypes[] layerTypes;
 
     public SerialController SC;
 
-    string recievedString;
+    GameObject layer_1,
+     layer_2,
+     layer_3,
+     layer_4,
+     layer_5;
+
+    GameObject[] layers;
+
+    string layer_1_SavedType,
+     layer_2_SavedType,
+     layer_3_SavedType,
+     layer_4_SavedType,
+     layer_5_SavedType;
+
+    string[] layer_SavedTypes;
+
+    Color32 Brown_color,
+     Yellow_color,
+     White_color,
+     Grey_color,
+     Red_color;
+
+    List<Color32> colorList;
 
     // Start is called before the first frame update
     void Start()
     {
         SC = GetComponent<SerialController>();
+
+        Brown_color = new Color32(107, 77, 26, 255);  //LayerTypePrefab_1.GetComponent<SpriteRenderer>().color;
+        Yellow_color = new Color32(105, 92, 19, 255); //LayerTypePrefab_2.GetComponent<SpriteRenderer>().color;
+        White_color = new Color32(84, 98, 40, 255);   //LayerTypePrefab_3.GetComponent<SpriteRenderer>().color;
+        Grey_color = new Color32(97, 85, 49, 255);    //LayerTypePrefab_4.GetComponent<SpriteRenderer>().color;
+        Red_color = new Color32(179, 51, 26, 255);    //LayerTypePrefab_5.GetComponent<SpriteRenderer>().color;
+
+        colorList = new()
+        {
+            Brown_color,
+            Yellow_color,
+            White_color,
+            Grey_color,
+            Red_color
+        };
+
+        layers = new GameObject[]
+        {
+            layer_1,
+            layer_2,
+            layer_3,
+            layer_4,
+            layer_5
+        };
+
+        layer_SavedTypes = new string[]
+        {
+            layer_1_SavedType,
+            layer_2_SavedType,
+            layer_3_SavedType,
+            layer_4_SavedType,
+            layer_5_SavedType
+        };
+
+        LayerPos = new GameObject[]
+        {
+            LayerPos_1,
+            LayerPos_2,
+            LayerPos_3,
+            LayerPos_4,
+            LayerPos_5
+        };
+
+        layerTypes = new LayerTypes[]
+        {
+            Layer_1_Type,
+            Layer_2_Type,
+            Layer_3_Type,
+            Layer_4_Type,
+            Layer_5_Type
+        };
+
+        for (int i = 0; i < layers.Length; i++)
+        {
+            BlockCreation(i + 1, -1);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (LayerTypes_1.ToString() != layer_1_SavedType)
-        {
-            Destroy(layer_1, 0.0f);
-
-            layer_1 = Instantiate(
-                FindLayerType(LayerTypes_1.ToString()),
-                LayerPos_1.transform.position,
-                Quaternion.identity);
-
-            layer_1_SavedType = LayerTypes_1.ToString();
-        }
-
-        if (LayerTypes_2.ToString() != layer_2_SavedType)
-        {
-            Destroy(layer_2, 0.0f);
-
-            layer_2 = Instantiate(
-                FindLayerType(LayerTypes_2.ToString()),
-                LayerPos_2.transform.position,
-                Quaternion.identity);
-
-            layer_2_SavedType = LayerTypes_2.ToString();
-        }
-
-        if (LayerTypes_3.ToString() != layer_3_SavedType)
-        {
-            Destroy(layer_3, 0.0f);
-
-            layer_3 = Instantiate(
-                FindLayerType(LayerTypes_3.ToString()),
-                LayerPos_3.transform.position,
-                Quaternion.identity);
-
-            layer_3_SavedType = LayerTypes_3.ToString();
-        }
-
-        if (LayerTypes_4.ToString() != layer_4_SavedType)
-        {
-            Destroy(layer_4, 0.0f);
-
-            layer_4 = Instantiate(
-                FindLayerType(LayerTypes_4.ToString()),
-                LayerPos_4.transform.position,
-                Quaternion.identity);
-
-            layer_4_SavedType = LayerTypes_4.ToString();
-        }
-
-        if (LayerTypes_5.ToString() != layer_5_SavedType)
-        {
-            Destroy(layer_5, 0.0f);
-
-            layer_5 = Instantiate(
-                FindLayerType(LayerTypes_5.ToString()),
-                LayerPos_5.transform.position,
-                Quaternion.identity);
-
-            layer_5_SavedType = LayerTypes_5.ToString();
-        }
-
-
-
+        string recievedString;
         recievedString = SC.ReadSerialMessage();
+        //recievedString = "5 84 98 40";    //White block
+        //recievedString = "4 96 96 32;"    //Brown block
+        //recievedString = "4 107 77 26";   //Brown block
+        //recievedString = "3 105 92 19";   //Yellow block
+        //recievedString = "3 136 85 34";   //Yellow block
+        //recievedString = "2 179 51 26";   //Red block
+        //recievedString = "1 97 85 49";    //Grey block
+        //recievedString = "1 78 98 39";    //Grey block
 
         if (recievedString == null)
             return;
@@ -137,36 +162,197 @@ public class Layer_Blocks_Controller : MonoBehaviour
         else            
         {
             Debug.Log("Message arrived: " + recievedString);
-      
-            int BlockNumber = recievedString[0];        
-            //Color();
+            ChangeBlock(recievedString);         
+        }
+    }
 
+    void BlockCreation(int layerNumber, int ColorIndex)
+    {
 
+        if (layerTypes[layerNumber - 1].ToString() != layer_SavedTypes[layerNumber - 1])
+        {
+            Destroy(layers[layerNumber - 1], 0.0f);
+
+            layers[layerNumber - 1] = Instantiate(
+                FindLayerTypePrefab(ColorIndex),
+                LayerPos[layerNumber - 1].transform.position,
+                Quaternion.identity
+            );
+
+            layer_SavedTypes[layerNumber - 1] = layerTypes[layerNumber - 1].ToString();
         }
 
     }
 
-    GameObject FindLayerType (string LayerType)
+    /*
+    void BlockCreation(int layerNumber)
+    {
+        BlockDestruction(layerNumber);
+
+        
+        if (Layer_1_Type.ToString() != layer_1_SavedType)
+        {
+            //BlockDestruction(1);
+
+            Destroy(layer_1, 0.0f);
+
+            layer_1 = Instantiate(
+                FindLayerTypePrefab(Layer_1_Type.ToString()),
+                LayerPos_1.transform.position,
+                Quaternion.identity);
+
+            layer_1_SavedType = Layer_1_Type.ToString();
+        }
+
+        if (Layer_2_Type.ToString() != layer_2_SavedType)
+        {
+            //BlockDestruction(2);
+
+            Destroy(layer_2, 0.0f);
+
+            layer_2 = Instantiate(
+                FindLayerTypePrefab(Layer_2_Type.ToString()),
+                LayerPos_2.transform.position,
+                Quaternion.identity);
+
+            layer_2_SavedType = Layer_2_Type.ToString();
+        }
+
+        if (Layer_3_Type.ToString() != layer_3_SavedType)
+        {
+            //BlockDestruction(3);
+
+            Destroy(layer_3, 0.0f);
+
+            layer_3 = Instantiate(
+                FindLayerTypePrefab(Layer_3_Type.ToString()),
+                LayerPos_3.transform.position,
+                Quaternion.identity);
+
+            layer_3_SavedType = Layer_3_Type.ToString();
+        }
+
+        if (Layer_4_Type.ToString() != layer_4_SavedType)
+        {
+            //BlockDestruction(4);
+
+            Destroy(layer_4, 0.0f);
+
+            layer_4 = Instantiate(
+                FindLayerTypePrefab(Layer_4_Type.ToString()),
+                LayerPos_4.transform.position,
+                Quaternion.identity);
+
+            layer_4_SavedType = Layer_4_Type.ToString();
+        }
+
+        if (Layer_5_Type.ToString() != layer_5_SavedType)
+        {
+            //BlockDestruction(5);
+
+            Destroy(layer_5, 0.0f);
+
+            layer_5 = Instantiate(
+                FindLayerTypePrefab(Layer_5_Type.ToString()),
+                LayerPos_5.transform.position,
+                Quaternion.identity);
+
+            layer_5_SavedType = Layer_5_Type.ToString();
+        }
+    }*/
+
+    void ChangeBlock(string recievedString)
+    {
+        string[] textSplit;
+
+        textSplit = recievedString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        int BlockNumber = int.Parse(textSplit[0]);
+        byte redColorNumber = byte.Parse(textSplit[1]);
+        byte greenColorNumber = byte.Parse(textSplit[2]);
+        byte blueColorNumber = byte.Parse(textSplit[3]);
+
+        //Debug.Log("BlockNumber: " + BlockNumber);
+        //Debug.Log("redColorNumber: " + redColorNumber);
+        //Debug.Log("greenColorNumber: " + greenColorNumber);
+        //Debug.Log("blueColorNumber: " + blueColorNumber);
+
+        Color32 inputColor = new Color32(redColorNumber, greenColorNumber, blueColorNumber, 255);
+
+        //Debug.Log("inputColor: " + inputColor);      
+
+        int indexInList = -1;
+
+        if (redColorNumber != 0 && greenColorNumber != 0 && blueColorNumber != 0)
+        {
+            indexInList = findClosestColor(colorList, inputColor);
+        } 
+
+        //closes match in RGB space
+        int findClosestColor(List<Color32> colors, Color32 target)
+        {
+            int colorDiffs = colors.Select(n => ColorDiff(n, target)).Min(n => n);
+            return colors.FindIndex(n => ColorDiff(n, target) == colorDiffs);
+        }
+
+        // distance in RGB space
+        int ColorDiff(Color32 c1, Color32 c2)
+        {
+            return (int)Math.Sqrt((c1.r - c2.r) * (c1.r - c2.r)
+                                 + (c1.g - c2.g) * (c1.g - c2.g)
+                                 + (c1.b - c2.b) * (c1.b - c2.b));
+        }
+
+        //string temp = "LayerTypes_" + BlockNumber.ToString();
+
+        //static object Parse (Type enumType, string value);
+
+        //layer_SavedTypes[BlockNumber - 1] = layerTypes[BlockNumber - 1].ToString();
+
+        //layerTypes[BlockNumber - 1] = Enum.Parse(typeof(LayerTypes).GetName(typeof(LayerTypes), indexInList));         
+
+        //Debug.Log("layerTypes[BlockNumber - 1]: " + layerTypes[BlockNumber - 1]);
+        //Debug.Log("(LayerTypes)indexInList: " + (LayerTypes)indexInList);
+
+        layerTypes[BlockNumber - 1] = (LayerTypes)indexInList;
+
+        //GetType().GetField("Layer_" + BlockNumber.ToString() + "_Type",
+        //    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public
+        //    ).SetValue(
+        //        this,
+        //        Enum.Parse(
+        //            typeof(LayerTypes),
+        //            Enum.GetName(
+        //                typeof(LayerTypes),
+        //                indexInList
+        //            )
+        //        )
+        //    );
+
+        BlockCreation(BlockNumber, indexInList);
+    }
+
+    GameObject FindLayerTypePrefab(int LayerType)
     {
         switch (LayerType)
         {
-            case "Dirt_Layer":
-                return LayerTypePrefab_1;
+            case 0:
+                return LayerTypePrefab_1;              
                 
-            case "Sand_Layer":
+            case 1:
                 return LayerTypePrefab_2;
                 
-            case "Chalk_Layer":
+            case 2:
                 return LayerTypePrefab_3;
                 
-            case "Stone_Layer":
+            case 3:
                 return LayerTypePrefab_4;
                 
-            case "Clay_Layer":
+            case 4:
                 return LayerTypePrefab_5;
                 
             default:
-                return LayerTypePrefab_1;
+                return LayerTypePrefab_0;
                
         }
     }
